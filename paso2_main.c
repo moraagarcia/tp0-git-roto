7ee40f6534e10d3f664b9285e56814e1794b242c
moraagarcia <moragarcia2007@hotmail.com>
2020-04-18 15:39:02 -0300

    agrego paso2

---
+++ paso2_main.c
@@ -0,0 +1,31 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include "paso2_wordscounter.h"
+
+#define SUCCESS 0
+#define ERROR -1
+
+int main(int argc, char* argv[]) {
+    FILE* input;
+    if (argc > 1) {
+        char filepath[30];
+        memcpy(filepath, argv[1], strlen(argv[1]) + 1);
+        input = fopen(filepath, "r");
+    } else {
+        input = stdin;
+    }
+
+    if (! input) {
+        return ERROR;
+    } else {
+        wordscounter_t counter;
+        wordscounter_create(&counter);
+        wordscounter_process(&counter, input);
+        size_t words = wordscounter_get_words(&counter);
+        printf("%zu\n", words);
+        wordscounter_destroy(&counter);
+        return SUCCESS;
+    }
+}
+
